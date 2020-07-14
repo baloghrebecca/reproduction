39369d48042f45a93efbe12a47f7c12c8576bad3
rebecca <balogh.rebecca@yahoo.de>
2020-07-14 15:32:49 +0200

    working mobile slider

--- src/components/galleryContainer.js
+++ src/components/galleryContainer.js
@@ -113,7 +113,7 @@ export default class GalleryContainer extends React.Component {
         endOfRight: this.slider.current.getBoundingClientRect().right
       })
     }
-    console.log('position', this.state.currentPosition, 'left', this.state.endOfLeft, 'right', this.state.endSlidesRight);
+    // console.log('position', this.state.currentPosition, 'left', this.state.endOfLeft, 'right', this.state.endSlidesRight);
     if (e.keyCode === 37) {
       if (this.state.currentPosition > this.state.endOfLeft) {
         this.setState({ currentPosition: this.state.endOfLeft })
@@ -142,40 +142,46 @@ export default class GalleryContainer extends React.Component {
   }
 
   handleTouchStart = (e) => {
-    console.log('touch start');
     const { touches } = e
     if (touches && touches.length === 1) {
+      console.log(this.state.currentPosition);
+
       const touch = touches[0]
       const startX = touch.clientX
-      this.setState({posX1: startX})
+      this.setState({ posX1: startX })
     }
   }
 
   handleTouchMove = (e) => {
     console.log('touchmove');
-        const posX2 = this.state.posX1 - e.touches[0].clientX;
-        this.setState({
-          posX2: posX2,
-        })
-        //the current left position of the slider 
-        const sliderPositionLeft = this.slider.current.offsetLeft
-        //get new position by subtracting the travelled pixel 
-        const position = sliderPositionLeft - posX2
-        this.setState({
-          currentPosition: position
-        })
+    const posX2 = Math.floor(this.state.posX1 - e.touches[0].clientX)
+    const sliderPositionLeft = this.slider.current.offsetLeft
+    const position = Math.floor(sliderPositionLeft - posX2)
+    this.setState({
+      posX2: posX2,
+      currentPosition: position
+    })
+
+    console.log('endleft', this.state.endOfLeft, 'endRight', this.state.endOfRight, 'current position', -this.state.currentPosition)
   }
 
   handleTouchEnd = () => {
-    console.log('end');
+    const { currentPosition, endOfLeft, endOfRight} = this.state
+    if (currentPosition > endOfLeft) {
+      this.setState({
+        currentPosition: endOfLeft
+      })
+    }
+    if (endOfRight < -currentPosition) {
+      this.setState({
+        currentPosition: endOfLeft
+      })
+    }
   }
 
   render() {
     return (<>
       <div onLoad={this.handleOnLoad}
-        onTouchStart={this.handleTouchStart}
-        onTouchEnd={this.handleTouchEnd}
-        onTouchMove={this.handleTouchMove}
         onKeyDown={this.handleKeyDown}
         onTouchMove={this.handleDrag}
         onTouchEnd={this.handleDragEnd}
@@ -183,7 +189,11 @@ export default class GalleryContainer extends React.Component {
         onClick={this.handleDragEnd}
         onMouseUp={this.handleDrag}
         onMouseDown={this.handleDragStart}
+        onTouchStart={this.handleTouchStart}
+        onTouchEnd={this.handleTouchEnd}
         ref={this.container}
+        onTouchMove={this.handleTouchMove}
+        style={{ overflow: this.state.overflow }}
         id="galleryProductPage">
         <div
           draggable="false"
