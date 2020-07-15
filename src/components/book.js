768e4e7e803a166c14233f0e15d6663e2ef6590f
rebecca <balogh.rebecca@yahoo.de>
2020-07-15 11:46:05 +0200

    working mobile books

--- src/components/book.js
+++ src/components/book.js
@@ -6,15 +6,21 @@ import ProductImages from './getProductImages'
 
 const Book = (props) => {
     const [index, setIndex] = useState(0);
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth)
     const bookRender = useRef()
 
     const handleMouseOver = (e) => {
         const { sliderLength } = props
+        //get the x position of the element 
         const x = e.nativeEvent.offsetX
+        //get elements width
         const width = e.target.offsetWidth
+        //base to calculate how much the mouse is on the way across 
         const percentage = x / width
-        const imageNumber = Math.floor(percentage * props.sliderLength)
-        console.log('img nr', imageNumber, '%', percentage, 'length', sliderLength-1);
+        //split images across the width of the element with the base
+        //so we devide the element and set an image according to the percentage the mouse is moving across the element
+        const imageNumber = Math.floor(percentage * sliderLength)
+        
         if (imageNumber > sliderLength-1 || imageNumber === sliderLength) {
             setIndex(sliderLength-1)
             return;
@@ -27,17 +33,39 @@ const Book = (props) => {
         }
     }
 
-    const handleMouseOut = () => {
-        const { sliderLength, images } = props
-    }
+    //update on window width resize
+    useEffect(() => {
+        if (window.innerWidth != windowWidth) {
+            setWindowWidth(window.innerWidth)
+        }
+    });
 
-    const handleTouchStart = () => {
+    const handleTouchMove = (e) => {
+        const { sliderLength } = props
+        //get the x position of the element 
+        const x = e.touches[0].clientX
+        //get elements width
+        const width = e.currentTarget.offsetWidth
+        console.log('width',);
+        //base to calculate how much the mouse is on the way across 
+        const percentage = x / width
+        const imageNumber = Math.floor(percentage * sliderLength)
+        if (imageNumber > sliderLength-1 || imageNumber === sliderLength) {
+            setIndex(sliderLength-1)
+            return;
+        }
+        if (x < 0) {
+            setIndex(0)
+            return;
+        } else {
+            setIndex(imageNumber)
+        }
 
     }
 
     return (
         <div className="book">
-            <div onTouchStart={handleTouchStart} onMouseOut={handleMouseOut}  onMouseMove={handleMouseOver} ref={bookRender} className="bookRenderingContainer">
+            <div onTouchMove={handleTouchMove} onMouseMove={handleMouseOver} ref={bookRender} className="bookRenderingContainer">
                 <img src={props.images[index].props.children.props.src} />
             </div>
             <h2 className="bookTitle">FOUNTAIN’S EDIT</h2>
