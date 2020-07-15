045a11359afef5685472447bd82ff9d68c710644
rebecca <balogh.rebecca@yahoo.de>
2020-07-15 16:37:26 +0200

    another bugfix

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import '../styles/index.scss';
 import LayoutNoMargin from '../components/layoutWithoutMarginBottom';
-import Cart from '../components/cart'
+import Cart from '../components/Cart'
 import HeaderMain from '../components/headerMain'
 
 export default class CartPage extends React.Component {
