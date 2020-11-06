668fa7270d947f82f25f71e4c1a22f59a51fdb40
rebecca <balogh.rebecca@yahoo.de>
2020-11-06 21:51:36 +0100

    back

--- src/services/checkout.js
+++ src/services/checkout.js
@@ -56,7 +56,7 @@ export const Checkout = (props) => {
                 allowedCountries: ['US', 'CA', 'AL', 'AD', 'AM', 'AT', 'BY', 'BE', 'BA', 'BG', 'CH', 'CY', 'CZ', 'DE',
                 'DK', 'EE', 'ES', 'FO', 'FI', 'FR', 'GB', 'GE', 'GI', 'GR', 'HU', 'HR',
                 'IE', 'IS', 'IT', 'LI', 'LT', 'LU', 'LV', 'MC', 'MK', 'MT', 'NO', 'NL', 'PL',
-                'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'UK', 'VA',],
+                'PT', 'RO', 'RU', 'SE', 'SI', 'SK', 'SM', 'TR', 'UA', 'VA',],
             }
         })
         if (error) {
